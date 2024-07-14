56891ea9dcff965c83e4aaba5c341842edb23487
ChoirulU <139779742+ChoirulU@users.noreply.github.com>
2024-07-14 21:59:04 +0700

    Pertemuan 12
    Menampilkan Data Backend: Register dan Login di Frontend

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import IndukMahasiswa from "./Induk-mahasiswa";
-import Home from "./assets/Pages/Home";
+// import Home from "./assets/Pages/Home";
 import { Route, Routes } from "react-router-dom";
 import State from "./assets/Pages/State";
 import EventHandling from "./assets/Pages/Handling";
@@ -9,11 +9,14 @@ import FetchApi from "./assets/Pages/Fetch";
 import ReactContext from "./assets/Pages/ReactContext";
 import GetMahasiswa from "./assets/componen/admin/GetMahasiswa";
 import CreateMahasiswa from "./assets/componen/admin/CreateMahasiswa";
+import Login from "./assets/componen/auth/Login";
+import Register from "./assets/componen/auth/Register";
+import DataMahasiswa from "./assets/componen/admin/DataMahasiwa";
 
 function App() {
   return ( 
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login/>} />
       <Route path="/IndukMahasiswa" element={<IndukMahasiswa />} />
       <Route path="/State" element={<State />} />
       <Route path="/Handling" element={<EventHandling/>} />
@@ -24,6 +27,9 @@ function App() {
 
       <Route path="/GetMahasiswa" element={<GetMahasiswa/>} />
       <Route path="/create" element={<CreateMahasiswa/> } />
+
+      <Route path="/register" element={<Register/> } />
+      <Route path="/admin/data-mahasiswa" element={<DataMahasiswa/> } />
     </Routes>
    );
 }
